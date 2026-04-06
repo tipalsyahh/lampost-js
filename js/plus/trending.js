f82365c6f3447a8095f040a36c25d953dbd4fb5b
tipalsyahh <145719681+tipalsyahh@users.noreply.github.com>
2026-04-06 12:35:57 +0700

    Change heading from 'Update Lampung Post' to 'Colom Pakar'

--- js/plus/trending.js
+++ js/plus/trending.js
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', async () => {
       <div class="lingkar-wrapper">
         <div class="lingkar-bg"></div>
         <div class="lingkar-panel">
-          <h3>Update Lampung Post</h3>
+          <h3>Colom Pakar</h3>
           <div class="lingkar-list">
             ${list}
           </div>
