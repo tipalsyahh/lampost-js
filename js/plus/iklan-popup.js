f4d159217607a7fb64905c043951fbadc079bda8
tipalsyahh <jinanaura@gmail.com>
2026-04-23 14:54:39 +0700

    js-lampost

---
+++ js/plus/iklan-popup.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const popup = document.getElementById("popupIklan");
+    const closeBtn = document.getElementById("popupClose");
+
+    setTimeout(() => {
+        popup.classList.add("active");
+    }, 3000);
+
+    closeBtn.addEventListener("click", () => {
+        popup.classList.remove("active");
+    });
+
+    popup.addEventListener("click", (e) => {
+        if (e.target === popup) {
+            popup.classList.remove("active");
+        }
+    });
+
+});
