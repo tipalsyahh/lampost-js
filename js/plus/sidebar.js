e973ffad51c22a0f7190d2388e4b510f2e10f3e1
tipalsyahh <jinanaura@gmail.com>
2026-04-23 09:12:33 +0700

    js-lampost

--- js/plus/sidebar.js
+++ js/plus/sidebar.js
@@ -75,7 +75,7 @@ const SIDEBAR = `
             <li id="link-mobile"><a href="https://lampost.co/kategori/breaking-news">Breaking-news</a></li>
             <li id="link-mobile"><a href="https://lampost.co/kategori/lampung">Lampung</a></li>
             <li id="link-mobile"><a href="https://lampost.co/kategori/teknologi">Teknologi</a></li>
-            <li><a href="https://lampost.co/kategori/iklan-pengumuman">Iklan Pengumuman</a></li>
+            <li class="has-sub"><a href="https://lampost.co/kategori/iklan-pengumuman">Iklan Pengumuman</a></li>
             <li><a href="https://lampost.co/indeks">Indeks</a></li>
         </ul>
     </nav>
@@ -175,7 +175,7 @@ const SIDEBAR = `
                     <li><a href="https://lampost.co/kategori/lampung/pesisir-barat">Pesisir Barat</a></li>
                     </ul>
                 </li>
-                <li class="has-sub"><a href="https://lampost.co/kategori/iklan-pengumuman"><i class="bi bi-megaphone"></i> Iklan Pengumuman</a></li>
+                <li><a href="https://lampost.co/kategori/iklan-pengumuman"><i class="bi bi-megaphone"></i> Iklan Pengumuman</a></li>
             </ul>
         </div>
     </div>
