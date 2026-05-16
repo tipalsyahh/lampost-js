3761fd03382a359fde8753446a0ae75a15e2e586
tipalsyahh <jinanauraa@gmail.com>
2026-05-16 10:41:05 +0700

    js lampost

--- js/plus/inculede.js
+++ js/plus/inculede.js
@@ -1,3 +1,67 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.body.insertAdjacentHTML("afterbegin", SIDEBAR);
 });
+
+const searchBtn = document.getElementById('search-baru');
+const searchModal = document.getElementById('searchModal');
+const closeSearchModal = document.getElementById('closeSearchModal');
+const searchOverlay = document.querySelector('.search-modal-overlay');
+const searchInput = document.querySelector('.search-modal-form input');
+
+/* =========================================
+OPEN
+========================================= */
+
+searchBtn.addEventListener('click', function(e){
+
+    e.preventDefault();
+
+    searchModal.classList.add('active');
+
+    document.body.style.overflow = 'hidden';
+
+    setTimeout(() => {
+
+        searchInput.focus();
+
+    }, 300);
+
+});
+
+/* =========================================
+CLOSE FUNCTION
+========================================= */
+
+function closeSearch(){
+
+    searchModal.classList.remove('active');
+
+    document.body.style.overflow = '';
+
+}
+
+/* =========================================
+BUTTON CLOSE
+========================================= */
+
+closeSearchModal.addEventListener('click', closeSearch);
+
+/* =========================================
+OVERLAY CLOSE
+========================================= */
+
+searchOverlay.addEventListener('click', closeSearch);
+
+/* =========================================
+ESC CLOSE
+========================================= */
+
+document.addEventListener('keydown', function(e){
+
+    if(e.key === 'Escape'){
+
+        closeSearch();
+
+    }
+
+});
