7887f87e4f07c9259789798738332d1ab8926c53
tipalsyahh <jinanauraa@gmail.com>
2026-05-29 13:27:23 +0700

    js lampost

--- js/plus/iklan-popup.js
+++ js/plus/iklan-popup.js
@@ -3,13 +3,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const popup = document.getElementById("popupIklan");
     const closeBtn = document.getElementById("popupClose");
 
+    // Popup tidak ada (status OFF)
+    if (!popup) return;
+
     setTimeout(() => {
         popup.classList.add("active");
     }, 6000);
 
-    closeBtn.addEventListener("click", () => {
-        popup.classList.remove("active");
-    });
+    if (closeBtn) {
+        closeBtn.addEventListener("click", () => {
+            popup.classList.remove("active");
+        });
+    }
 
     popup.addEventListener("click", (e) => {
         if (e.target === popup) {
